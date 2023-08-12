560ffc41737afda31efd0f9f75a8e024f8410653
Fabian <fabian.ochoa@protonmail.com>
2023-08-12 02:28:19 -0700

    Added app.server()

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import dash_bootstrap_components as dbc
 from dash import Dash
-import nfl_data_py as nfl
 
 from src.components.layout import create_layout
 
@@ -9,7 +8,8 @@ def main() -> None:
     app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
     app.title = "NFL Stats Analyzer"
     app.layout = create_layout(app)#, data)
+    server = app.server()
     app.run(debug=True)
 
 if __name__ == '__main__':
-    main()
+    main()  
